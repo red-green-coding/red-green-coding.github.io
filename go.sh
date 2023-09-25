1f7e23ff346b82a265a701b0ec4df6ae42c388a2
Martin Möller <normartin@users.noreply.github.com>
2023-09-25 12:06:09 +0200

    use docker build

--- go.sh
+++ go.sh
@@ -5,10 +5,40 @@ set -e
 
 export JEKYLL_VERSION=3.8
 
-docker run --rm \
-  --volume="$PWD:/srv/jekyll:Z" \
-  --volume="$PWD/vendor/bundle:/usr/local/bundle:Z" \
-  --env FORCE_POLLING=true \
-  -p 4000:4000 \
-  -it jekyll/builder:$JEKYLL_VERSION \
-  jekyll $1
+goal_serve() {
+  docker run --rm \
+    --volume="$PWD:/srv/jekyll:Z" \
+    --volume="$PWD/vendor/bundle:/usr/local/bundle:Z" \
+    --env FORCE_POLLING=true \
+    -p 4000:4000 \
+    -it jekyll/builder:$JEKYLL_VERSION \
+    jekyll serve
+}
+
+goal_build() {
+  docker run --rm \
+    --volume="$PWD:/srv/jekyll:Z" \
+    --volume="$PWD/vendor/bundle:/usr/local/bundle:Z" \
+    --env JEKYLL_ENV=production \
+    jekyll/builder:$JEKYLL_VERSION \
+    jekyll build
+}
+
+goal_help() {
+  echo "usage: $0 <goal>
+    available goals
+
+    "
+  exit 1
+}
+
+main() {
+  local TARGET=${1:-}
+  if [ -n "${TARGET}" ] && type -t "goal_$TARGET" &>/dev/null; then
+    "goal_$TARGET" "${@:2}"
+  else
+    goal_help
+  fi
+}
+
+main "$@"
