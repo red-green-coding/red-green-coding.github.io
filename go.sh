563533ed4e9d96645bfe4814cb7e788ac77712b1
Martin Möller <normartin@users.noreply.github.com>
2023-10-16 10:42:28 +0200

    Use plain ruby image

--- go.sh
+++ go.sh
@@ -2,25 +2,25 @@
 
 set -e
 
-export JEKYLL_VERSION=3.8
-
 goal_serve() {
-  docker run --rm \
-    --volume="$PWD:/srv/jekyll:Z" \
-    --volume="$PWD/vendor/bundle:/usr/local/bundle:Z" \
-    --env FORCE_POLLING=true \
-    -p 4000:4000 \
-    -it jekyll/builder:$JEKYLL_VERSION \
-    jekyll serve --drafts
+  docker run \
+      --volume="$PWD:/usr/src/app" \
+      --volume="$PWD/vendor/bundle/gems:/usr/local/bundle" \
+      --workdir=/usr/src/app \
+      -p 4000:4000 \
+      -it ruby:2.7 \
+      bash -c "bundle install && bundle exec jekyll serve --host 0.0.0.0"
 }
 
 goal_build() {
-  docker run --rm \
-    --volume="$PWD:/srv/jekyll:Z" \
-    --volume="$PWD/vendor/bundle:/usr/local/bundle:Z" \
-    --env JEKYLL_ENV=production \
-    jekyll/builder:$JEKYLL_VERSION \
-    jekyll build
+  docker run \
+      --volume="$PWD:/usr/src/app" \
+      --volume="$PWD/vendor/bundle/gems:/usr/local/bundle" \
+      --workdir=/usr/src/app \
+      --env JEKYLL_ENV=production \
+      -p 4000:4000 \
+      -it ruby:2.7 \
+      bash -c "bundle install && bundle exec jekyll build"
 }
 
 goal_help() {
